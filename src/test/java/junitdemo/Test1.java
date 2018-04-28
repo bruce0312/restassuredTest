49d46c6ecedc1bbb3c53a93bfe4a2a3911e694ba
bruce <bruceshen312@gmail.com>
2018-04-28 23:48:11 +0800

    interfaceTest1

---
+++ src/test/java/junitdemo/Test1.java
@@ -0,0 +1,42 @@
+package junitdemo;
+
+import org.junit.*;
+
+public class Test1 {
+
+    @BeforeClass
+    public static void beforeClass(){
+        System.out.println("this is beforeClass test");
+    }
+
+    @Before
+    public void before(){
+        System.out.println("this is before test");
+    }
+
+
+    @Test
+    public void fun1(){
+        System.out.println("this is a fun1 test");
+    }
+
+    @Test
+    public void fun2(){
+        System.out.println("this is a fun2 test");
+    }
+
+    @After
+    public void after(){
+        System.out.println("this is after test");
+    }
+
+    @AfterClass
+    public static void afterClass(){
+        System.out.println("this is afterClass test");
+    }
+
+
+    public static void main(String[] args){
+
+    }
+}
---
+++ src/test/java/suite/DemoTestSuite.java
@@ -0,0 +1,21 @@
+package suite;
+
+
+import junitdemo.Test1;
+import junitdemo.Test2;
+import junitdemo.Test3;
+import junitdemo.Test4;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        Test1.class,
+        Test2.class,
+        Test3.class,
+        Test4.class
+})
+
+public class DemoTestSuite {
+
+}
